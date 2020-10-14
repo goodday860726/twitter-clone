24649560a18350c304cf760adee32662b8bee988
YOUR NAME <YOUR@EMAIL.com>
2020-10-14 20:47:19 +0900

    Updates

---
+++ precache-manifest.c9e2ec182a402ded6e4f541ca407fb66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2a36c458ac17455d22d34f1d657bb9",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "aabba29d4410e264f39c",
+    "url": "/twitter-clone/static/css/main.156e8e89.chunk.css"
+  },
+  {
+    "revision": "4a5f7b6ead863bd265c3",
+    "url": "/twitter-clone/static/js/2.7742996c.chunk.js"
+  },
+  {
+    "revision": "c632640d265671a6e6eee1edde0b38ef",
+    "url": "/twitter-clone/static/js/2.7742996c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabba29d4410e264f39c",
+    "url": "/twitter-clone/static/js/main.39bc6d8f.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goodday860726/twitter-clone.git/precache-manifest.65855f0868dd3e059a89043e8f5bcfaf.js"
+  "/twitter-clone/precache-manifest.c9e2ec182a402ded6e4f541ca407fb66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goodday860726/twitter-clone.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitter-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
